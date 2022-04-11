d77cefc71ecf4cd0001cb47ab8fc6e3ace6ead3e
becket <becket.bowes@gmail.com>
2022-04-10 23:56:15 -0400

    aaah back to codes

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,54 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+    //get all pies
+    getAllPizza(req, res) {
+        Pizza.find({})
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => { console.log(err); res.status(400).json(err); });
+    },
+    //get a pie
+    getPizzaByID({ params }, res) {
+        Pizza.findOne({ _id: params.id })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'Ho! Whersa tha Pizza?' });
+                return;
+            }
+            res.json(dbPizzaData)
+        })
+        .catch(err => { console.log(err); res.status(400).json(err); });
+    },
+    //maka da pie
+    createPizza({ body }, res) {
+        Pizza.create(body)
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.status(400).json(err));
+    },
+    //fixa da pie
+    updatePizza({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'Ho! Who tooka da pizza?' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    //tossa da pie
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({ _id: params.id })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'we don hava da pizza' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+};
+
+module.exports = pizzaController;
